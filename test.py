9bf6174716bda40427e01a5503f7a29349bd7395
MoMahdi1 <97743652+MoMahdi1@users.noreply.github.com>
2026-08-12 23:07:13 +0300

    updates backend

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
+
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
 
 
-res = search_flights("Plan a 7 days trip to Paris from Cairo")
-
-print(res)
+print("\nFINAL RESPONSE:\n")
+print(response['answer'])
