262ce9bdde4f00cede2acf664a25fe03badb44a6
MoMahdi1 <97743652+MoMahdi1@users.noreply.github.com>
2026-08-12 18:40:37 +0300

    flight and tavily tools added

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
 
 
-
-res = tavily_search("Best hotels in Cairo")
+res = search_flights("Plan a 7 days trip to Paris from Cairo")
 
 print(res)
